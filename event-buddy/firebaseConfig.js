61e1bce3f5ab1af9809a30e389537ff11bdcb9e7
Helder Lima <104677439+HelderRSimao@users.noreply.github.com>
2025-07-07 20:58:42 +0100

    Update firebaseConfig.js

--- event-buddy/firebaseConfig.js
+++ event-buddy/firebaseConfig.js
@@ -7,12 +7,12 @@ import "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1RbOo8qip11EFAR17AgWShTUM8s0SKRg",
-  authDomain: "testdb-f2fa6.firebaseapp.com",
-  projectId: "testdb-f2fa6",
-  storageBucket: "testdb-f2fa6.firebasestorage.app",
-  messagingSenderId: "160410285858",
-  appId: "1:160410285858:web:c658524cadbb26f9abd77b"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
  
